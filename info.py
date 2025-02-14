95bf977653c3e25f28f27350a3b6c740707a72c7
Devensh22345 <kumardevensh0@gmail.com>
2025-02-15 01:39:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
 API_ID       = 22207976
 API_HASH     = "5c0ad7c48a86afac87630ba28b42560d"
 BOT_TOKEN    = 7732000256:AAFaxCLQ7yJ_wvrO6pDp8KLxXf5HHv4cpQ4""
-SESSION      = "BQAncJb047wcFSUie2bbX4nDIUqVdyowoioe4e0Jd71nXMS4Sxzr2GCQr6fjn_Eo_SXgvQAgevOUgVmlqr5pQnreYGgoYuEFFlEj5jx9rFoVwgpIeN-fQbTmDuRf8od5Qkw0qMe6OxmDsdO4906f7CoRnMSweZxjQhiOr7PClarkoGJfzahPgIFnmyNrUPh4wzy0i_EjS2-SJhy3ZZbPqxC4sjD0EJ38lTYro4PYe6xMnplopkcPi58eIinrRKVsHlw3s2z0WoJJkKn23QopP4yg96KfNQyHROy7teEduPpBQHwHPJXHFgx5xaQYrS8iVBLAXcozvPlqWD_rInseH64fAAAAAVUN5igA"
+SESSION      = "BQF8ngYAd6VJLfS5DgJgf_i8ZNmhlPrbkMuSUImdpxKr-m5_nGTfz-v6jKLHJZrR13Um75D9VdM8Oy9kVp-_rgmIGCrDdYItTa0-qsvIz0ElYyudXcGkLZBLAK5gl-mD45nLF_wyfWg9VryJ9VDMfbwyIMReiLrZxSn5aPNWdIblg_g7Q3Hqr2G1TbW2cSC4KzHMHJEoxndmVKudu9tCne6jsACLznXhnPrKgKC6O6lh-O9rUbePgC7g0q9p8d-yJJETaVhA-Tm8RPzxxdcCIKADS_WtMEs0jU6FwEz5gxZLbQozmahLw38PWYm_hFWZy7UOd2N69CDMlwXtkQPqrzV8rH0bXQAAAAGi62njAA"
 DATABASE_URI = "mongodb+srv://Autofilterbot:Autofilterbot@autofilterbot.evsptxd.mongodb.net/?retryWrites=true&w=majority&appName=Autofilterbot"
 LOG_CHANNEL  = -1001942398843
 ADMIN        = 6872968794
